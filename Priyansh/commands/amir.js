75b94c142fa5c0eccd7c6c19d5809df85943539c
Shaanbot1234 <b8465391@gmail.com>
2024-12-06 12:19:20 +0500

    Update amir.js

--- Priyansh/commands/amir.js
+++ Priyansh/commands/amir.js
@@ -1,1 +1,36 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "react",
+    version: "1.1.1",
+	hasPermssion: 0,
+	credits: "Muhammad Ali", 
+	description: "Just Respond",
+	commandCategory: "no prefix",
+    cooldowns: 5, 
+};
 
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("owner") ||
+     react.includes("shan") || react.includes("shaan") || react.includes("Shan") ||
+react.includes("Shaan") ||
+react.includes("admin")) {
+		var msg = {
+				body: `★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★
+               ✦𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍✦
+
+☞𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞:
+https://www.facebook.com/𝚜𝚑𝚊𝚊𝚗 uid👉✨100016828397863✨
+
+
+
+★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  ✦𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊✦`,attachment: fs.createReadStream(__dirname + `/noprefix/khan.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("💔", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
