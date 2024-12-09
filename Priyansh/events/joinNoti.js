2eb817ce7e9fd112172867da873ad28e171d618e
Shaanbot1234 <b8465391@gmail.com>
2024-12-09 19:57:52 +0500

    Update joinNoti.js

--- Priyansh/events/joinNoti.js
+++ Priyansh/events/joinNoti.js
@@ -59,7 +59,7 @@ https://www.instagram.com/shankhank345?igsh=MXVneXZ6bzR4bHhvcg==￼
 
 ☣️Email: shankhank345@gmail.com
 
-⚠️Wp: +92 3368783346 `, attachment: fs.createReadStream(__dirname + "/cache/DhFHY1LcrjWZwBRkMlrtOdHr7QOeiGFMZqBGa4AEErRO.mp4")} ,threadID));
+⚠️Wp: +92 3368783346 `, attachment: fs.createReadStream(__dirname + "/cache/Mivo1707223546393.mp4")} ,threadID));
         }
         else {
                 try {
