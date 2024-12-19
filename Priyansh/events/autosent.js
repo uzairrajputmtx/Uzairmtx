7ab08f8fb1cb020d91d295c49284aa2a98ff5576
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-19 21:36:49 +0500

    Update autosent.js

--- Priyansh/events/autosent.js
+++ Priyansh/events/autosent.js
@@ -2,13 +2,13 @@ module.exports.config = {
  name: "autosend",
  eventType: [],
  version: "0.0.1",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+ credits: "uzairrajput",
  description: "Listen events"
 };
 
 module.exports.run = async({ event, api, Threads, Users }) => {
 const moment = require("moment-timezone");
-time = moment.tz('Asia/Kolkata').format('HH:mm:ss');
+time = moment.tz('Asia/Karachi').format('HH:mm:ss');
 var cantsend = [];
     var allThread = global.data.allThreadID || [];
     if (time == "16:52:00") {
