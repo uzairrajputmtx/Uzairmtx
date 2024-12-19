ff1cf0c2e16213682df7d7cc37c64a8b0178152f
uzairrajputmtx <uzairuzma449@gmail.com>
2024-12-19 21:38:02 +0500

    Update antirobbery.js

--- Priyansh/events/antirobbery.js
+++ Priyansh/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+    credits: "uzairrajput",
     description: "Prevent admin changes",
 };
 
